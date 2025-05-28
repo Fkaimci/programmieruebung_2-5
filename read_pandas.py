4e3b51c5f0ee6464517fcad3abd38fb87cc7075a
Olivia Kiechl <ko8973@mci4me.at>
2025-05-28 15:25:58 +0200

    28.05.2024

--- read_pandas.py
+++ read_pandas.py
@@ -2,7 +2,10 @@
 
 # Paket für Bearbeitung von Tabellen
 import pandas as pd
+import numpy as np
 
+import plotly.io as pio 
+pio.renderers.default = "browser"
 # Paket
 ## zuvor !pip install plotly
 ## ggf. auch !pip install nbformat
@@ -13,10 +16,13 @@ def read_my_csv():
     # Einlesen eines Dataframes
     ## "\t" steht für das Trennzeichen in der txt-Datei (Tabulator anstelle von Beistrich)
     ## header = None: es gibt keine Überschriften in der txt-Datei
-    df = pd.read_csv("data/ekg_data/01_Ruhe.txt", sep="\t", header=None)
+    df = pd.read_csv("data/activities/activity.csv")
+                   
 
+    time = np.arange(0, len(df))
+    df["Time"] = time
     # Setzt die Columnnames im Dataframe
-    df.columns = ["Messwerte in mV","Zeit in ms"]
+    #df.columns = ["Messwerte in mV","Zeit in ms"]
     
     # Gibt den geladen Dataframe zurück
     return df
@@ -25,16 +31,20 @@ def read_my_csv():
 # %%
 
 def make_plot(df):
-
     # Erstellte einen Line Plot, der ersten 2000 Werte mit der Zeit aus der x-Achse
-    fig = px.line(df.head(2000), x= "Zeit in ms", y="Messwerte in mV")
+    fig = px.line(df, x="Time", y=["PowerOriginal", "HeartRate"])
     return fig
 
-#%% Test
+    df = pd.DataFrame(data)
 
-#df = read_my_csv()
-#fig = make_plot(df)
+  
+if __name__ == "__main__":
+    #Lese die Datei ein
+    df = read_my_csv()
+    print(df.head())
 
-#fig.show()
+    #Erstelle einen Plot
+    fig = make_plot(df)
 
-# %%
+    #Zeige den Plot an 
+    fig.show()
